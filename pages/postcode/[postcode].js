a77a29608e8dca862a47cbbc4d528309bb27c2d1
Paul Wilson <pwilson802@gmail.com>
2022-04-05 17:44:51 +1000

    added postcodes page for routing test

---
+++ pages/postcode/[postcode].js
@@ -0,0 +1,35 @@
+import SearchAppBar from "../components/SearchAppBar"
+import PostcodeCard from "../components/PostcodeCard"
+import {GetSiteData} from "../components/FetchData"
+import { useRouter } from 'next/router'
+
+
+export default function Home({ postcodes, latest }) {
+    const router = useRouter()
+    const { postcode } = router.query
+    return (
+        <>
+            <div>Postcode: {postcode }</div>
+        
+        </>
+    )
+
+
+}
+
+
+
+
+export async function getServerSideProps() {
+  let data = await GetSiteData()
+  let postcodes = data.postcodes
+  let latest = data.latest
+  console.log(data)
+
+  return {
+    props: {
+      postcodes,
+      latest,
+    },
+  };
+}
